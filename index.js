9138dcdb7207c0175067602506572cf2e2ecdc0d
Pratibha-Sharma1507 <sharmamunnu555@gmail.com>
2025-11-01 13:11:04 +0530

    add origin in index.js4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://optimizalpha-frontend.vercel.app"],
+      origin: ["https://optimizalpha-frontend.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
